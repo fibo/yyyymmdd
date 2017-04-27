f94b7898dbb755163b29c83993fc93a5bd50c334
fibo <casati_gianluca@yahoo.it>
2017-04-27 14:46:21 +0200

    disable highlight

--- yyyymmdd.sh
+++ yyyymmdd.sh
@@ -2,11 +2,11 @@ function yyyymmdd () {
     YYYYMM=$1
     if [ ${#YYYYMM} -ne 6 ]
     then
-        echo "Param $YYYYMM is not valid" 1>&2
+        echo "Param $YYYYMM is required in YYYYMM format" 1>&2
     else
         YYYY=${YYYYMM:0:4}
         MM=${YYYYMM:4:2}
-        for d in $(cal $MM $YYYY | grep "^ *[0-9]")
+        for d in $(cal -h $MM $YYYY | grep "^ *[0-9]")
         do
             DD=$(printf "%02d" $d)
             echo $YYYY$MM$DD
